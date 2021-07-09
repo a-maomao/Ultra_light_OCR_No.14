ca72bdd5ef0a723673dbe5b95adc59a3c1b32bcd
a-maomao <61502838+a-maomao@users.noreply.github.com>
2021-07-09 10:37:12 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 # recommended paddle.__version__ == 2.0.0
-python3 -m paddle.distributed.launch --log_dir=./debug/ --gpus '0,1,2,3,4,5,6,7'  tools/train.py -c configs/rec/rec_mv3_none_bilstm_ctc.yml
+python3.7 tools/train.py -c configs/rec/ch_ppocr_v2.0/rec_chinese_mv3_train_v2.0.yml
